2ee28ddc005a06156e1c7557673864ab498c7603
stephenkati <stephenkatuli88@gmail.com>
2023-08-01 19:29:07 +0300

    Implement general shopping list

--- app/controllers/general_shopping_lists_controller.rb
+++ app/controllers/general_shopping_lists_controller.rb
@@ -1,49 +1,18 @@
 class GeneralShoppingListsController < ApplicationController
   def index
     @user = current_user
-    @recipes = @user.recipes.includes(recipe_foods: :food)
-    @general_food_list = @user.foods
-    @shopping_list = shopping_list
-    @total_items = @shopping_list.size
-    @total_price = total_price
+    @general_shopping_list = generate_general_shopping_list
+    @total_food_items = @general_shopping_list.sum { |item| item[:quantity] }
+    @total_price = @general_shopping_list.sum { |item| item[:price] * item[:quantity] }
   end
 
   private
 
-  def shopping_list
-    shopping_list_raw = build_shopping_list_raw
+  def generate_general_shopping_list
+    unused_foods = current_user.foods.where.not(id: current_user.recipes.joins(:foods).distinct.pluck('foods.id'))
 
-    shopping_list = []
-    shopping_list_raw.each_value do |recipe_food|
-      general_food = @general_food_list.find_by(name: recipe_food[:food].name)
-      next unless general_food.nil? || general_food.quantity < recipe_food[:quantity]
-
-      quantity = recipe_food[:quantity] - (general_food&.quantity || 0)
-      shopping_list << {
-        food: recipe_food[:food],
-        quantity:,
-        price: (general_food&.price || 0) * quantity
-      }
+    unused_foods.map do |food|
+      { food_id: food.id, name: food.name, price: food.price, quantity: food.quantity }
     end
-
-    shopping_list
-  end
-
-  def build_shopping_list_raw
-    shopping_list_raw = {}
-
-    @recipes.each do |recipe|
-      recipe.recipe_foods.each do |recipe_food|
-        food = recipe_food.food
-        shopping_list_raw[food.id] ||= { food:, quantity: 0 }
-        shopping_list_raw[food.id][:quantity] += recipe_food.quantity
-      end
-    end
-
-    shopping_list_raw
-  end
-
-  def total_price
-    @shopping_list.sum { |item| item[:price] }
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :confirmable
 
+  has_many :foods
   has_many :recipes
 end
