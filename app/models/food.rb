0a2a4c974f7baf65d155e8e4919ef7b057687c02
Sasan Moshirabadi <sasan.dev89@gmail.com>
2023-08-01 14:02:56 +0300

    Add model validations

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,3 +1,9 @@
 class Food < ApplicationRecord
   belongs_to :user
+
+  # Validations
+  validate :name, presence: true, length: { max: 250}
+  validate :measurement_unit, presence: true
+  validate :price, presence: true
+  validate :quantity, presence: true
 end
