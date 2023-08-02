64936749ddaa649050e22b40dd46220123596545
Sasan Moshirabadi <sasan.dev89@gmail.com>
2023-08-02 11:07:15 +0300

    Fix ability file for user permissions & toggle_public authorization echeck

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -10,7 +10,8 @@ class Ability
 
     return unless user.present?
 
-    can :manage, Food, user:
+    can :manage, Food, user: user
+    can :manage, Recipe, user: user
 
     # return unless user.admin?
     # can :manage, :all
